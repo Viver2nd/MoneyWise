0e2a68e1c6228732125490013d2f746b7c745df0
ryan <rbulluss2000@gmail.com>
2023-08-03 21:46:23 +0100

    Create the main templates and set up a nav bar to move between them

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.dashboard, name='dashboard'),
+    path('accounts/', views.accounts, name='accounts'),
+    path('transactions/', views.transactions, name='transactions'),
+    path('budget/', views.budget, name='budget'),
+    path('settings/', views.settings, name='settings'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,27 @@
 from django.shortcuts import render, redirect
 
-def home(request):
-  return render(request, 'home.html')
+def dashboard(request):
+  # Pass through relevent data 
+  
+  return render(request, 'dashboard.html')
+
+def accounts(request):
+  # Pass through relevent data 
+
+  return render(request, 'accounts.html')
+
+def transactions(request):
+  # Pass through relevent data 
+
+  return render(request, 'transactions.html')
+
+def budget(request):
+  # Pass through relevent data 
+
+  return render(request, 'budget.html')
+
+def settings(request):
+  # Pass through relevent data 
+
+  return render(request, 'settings.html')
+
