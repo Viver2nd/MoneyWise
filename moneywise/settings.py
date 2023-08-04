9abefa84d98fb48f7a04fa33db0139c07bed87bf
ryan <rbulluss2000@gmail.com>
2023-08-04 14:49:07 +0100

    change the database variables

--- moneywise/settings.py
+++ moneywise/settings.py
@@ -84,10 +84,10 @@ WSGI_APPLICATION = 'moneywise.wsgi.application'
 DATABASES = {
   'default': {
     'ENGINE': 'django.db.backends.postgresql',
-    'NAME': 'MoneyWise',
+    'NAME': os.environ['DB_NAME'],
     'USER': os.environ['DB_USER'],
     'PASSWORD': os.environ['DB_PASSWORD'],
-    'HOST': 'ep-shrill-band-03847623.eu-central-1.aws.neon.tech',
+    'HOST': os.environ['DB_HOST'],
     'PORT': '5432',
   }
 }
