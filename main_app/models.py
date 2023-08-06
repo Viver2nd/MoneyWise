e8586195226dfd55c80eb38174cf4c1daf8a7462
ryan <rbulluss2000@gmail.com>
2023-08-06 17:13:20 +0100

    Display all transactions in the transactions page. Start on the financial calculator

--- main_app/models.py
+++ main_app/models.py
@@ -22,7 +22,7 @@ class Account(models.Model):
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
     def __str__(self):
-        return f'{self.name} ({self.id})'
+        return f'{self.name}: £{self.balance}'
     
     def get_absolute_url(self):
         return reverse('accounts')
